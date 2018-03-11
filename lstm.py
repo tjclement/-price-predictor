23f5c0f9cf6f6be61de15fa28c82cfcfbb06a595
Benjamin Wölfl <benjamin.woelfl@imc-krems.eu>
2018-03-11 16:41:01 +0100

    lstm model works now with look-back of 1

--- lstm.py
+++ lstm.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
-Author: Ben
-Date: 10 March
+KICKSTARTER: Ben
+DATE: 10-11 March
 """
 
-# import of libraries
+# IMPORT OF LIBRARIES
 import time
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,58 +18,83 @@ from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
-# start of total execution time measurement
-START_TIME = time.time()
 
-# constants
+# CONSTANTS
+START_TIME = time.time() # start of total execution time measurement
 TRAIN_PROPORTION = 0.8 # proportion of training set, 1 - TRAIN_PROPORTION == TEST_PROPORTION
-LOOK_BACK = 5
+LOOK_BACK = 5 # hyperparameter
 SEED = 0
-
-# fixation of random seed
-np.random.seed(SEED)
-
-# function definitions
+BATCHES = 1
+FEATURE_DIM = 4
+OUTPUT_DIM = 1
 
 
-def extract_features_and_outputs(dataset, look_back=LOOK_BACK):
+# FUNCTION DEFINITIONS
+def preprocess_dataset(_X, look_back=LOOK_BACK):
     # process dataset so that np.arrays of features and output are extracted
     x, y = [], [] # features (x), output (y)
-    for i in range(0, len(dataset)-look_back):
-        dataset_window = dataset[i:(i+look_back), :]
-        x.append(dataset_window)
-        y.append(dataset[i+look_back, -1])
-    return np.array(x), np.array(y)
+    y = _X[1:, 3]
+    for i in range(0, len(_X)-1):
+         x.append(_X[i, :])
+    return np.array(x[:len(x) - (len(x) % look_back)]), np.array(y[:len(y) - (len(y) % look_back)])
 
-
-# import of dataset
-dataset = pd.read_csv('./data/curated_dataset_1.csv', usecols=[1,2,3,4], engine='python').values\
-    .astype('float32') # raw <np.ndarray>
-
-# normalization of dataset (? should we keep)
-dataset = MinMaxScaler(feature_range=(0, 1)).fit_transform(dataset) # scaled <np.ndarray>
-
-# split of dataset into training and test dataset
-train_size = int(len(dataset) * TRAIN_PROPORTION)
-test_size = len(dataset) - train_size
-dataset_train, dataset_test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-
-# extraction of features (x) and output (y)
-train_x, train_y = extract_features_and_outputs(dataset_train)
-test_x, test_y = extract_features_and_outputs(dataset_test)
-
-print('Features:')
-print(test_x[-1])
-print('Output:')
-print(test_y[-1])
-
-# next step: reshape so that i can be processed in Keras LSTM
 
 def main():
-    pass
+    # fixation of random seed
+    np.random.seed(SEED)
 
+    # import of dataset X (change to curated_dataset_2.csv for the second dataset)
+    X = pd.read_csv('./data/curated_dataset_1.csv', usecols=[1, 2, 3, 4], engine='python').values \
+        .astype('float32')  # raw <np.ndarray>
+
+    # normalization of dataset (recommended for LSTM) (comment out to check numbers easier)
+    X = MinMaxScaler(feature_range=(0, 1)).fit_transform(X) # scaled <np.ndarray>
+
+    # split of dataset into training and test dataset
+    train_size = int(len(X) * TRAIN_PROPORTION)
+    test_size = len(X) - train_size
+    X_train, X_test = X[0:train_size, :], X[train_size:len(X), :]
+
+    # extraction of features (x) and output (y)
+    train_x, train_y = preprocess_dataset(X_train)
+    test_x, test_y = preprocess_dataset(X_test)
+
+    # reshaping (requirement for Keras input)
+    train_x = train_x.reshape((len(train_x), BATCHES, FEATURE_DIM))
+    test_x = test_x.reshape((len(test_x), BATCHES, FEATURE_DIM))
+
+    # LSTM construction
+    NEURONS_HIDDEN_LAYER_1 = 50 # hyperparameter
+    NEURONS_OUTPUT_LAYER = 1
+    LOSS_FUNCTION = 'mae' # hyperparameter
+    OPTIMIZER = 'adam' # let's keep it fixed
+    EPOCHS = 100 # hyperparameter
+    BATCH_SIZE = 50 # hyperparameter
+
+    model = Sequential()
+    model.add(LSTM(NEURONS_HIDDEN_LAYER_1, input_shape=(train_x.shape[1], train_x.shape[2])))
+    model.add(Dense(NEURONS_OUTPUT_LAYER))
+    model.compile(loss=LOSS_FUNCTION, optimizer=OPTIMIZER)
+
+    # LSTM fit: here test_x and test_y where used, although they are used as VALIDATION sets in this case
+    history = model.fit(train_x, train_y, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(test_x, test_y), verbose=2,
+                        shuffle=False)
+    # correction: segregate data into TRAIN (60%), VALIDATION (20%) and TEST (20%) set (roll-forward); (do in beginning)
+
+    # plot history
+    plt.plot(history.history['loss'], label='train')
+    plt.plot(history.history['val_loss'], label='test')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss (%s)' % LOSS_FUNCTION)
+    plt.legend()
+
+    print('Script was successfully executed in %.8s s.' % (time.time() - START_TIME))
+
+    plt.show()
+
+# SCRIPT EXECUTION
 if __name__ == '__main__':
     main()
 
-print('Script was successfully executed in %.8s s.' %(time.time() - START_TIME))
-# end of script
+
+# END OF FILE
